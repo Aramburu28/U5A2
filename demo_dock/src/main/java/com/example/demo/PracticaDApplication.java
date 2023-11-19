511f5bea04cb5a955983bbc09c7fe7b0b74d9216
Aramburu28 <125260633+Aramburu28@users.noreply.github.com>
2023-11-19 23:07:39 +0100

    Add files via upload

---
+++ demo_dock/src/main/java/com/example/demo/PracticaDApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDApplication.class, args);
+	}
+
+}
